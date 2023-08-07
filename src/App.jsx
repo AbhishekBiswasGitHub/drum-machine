c38d4693976884de815cabe8168b4648b6cf061f
abbup <abhishek.biswas.git@gmail.com>
2023-08-07 18:31:15 +0530

    Add initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 import "./App.css";
 
 const App = () => {
-  return <></>;
+  const [display, setDisplay] = useState("");
+
+  const handleDisplay = (id) => {
+    setDisplay(id);
+  };
+
+  return <div id="drum-machine"></div>;
 };
 
 export default App;
